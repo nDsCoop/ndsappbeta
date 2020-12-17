fe88ec5bac794c12b685a45df0cd0ecf862307bc
nDsCoop <nds.tphcm@gmail.com>
2020-12-18 05:12:49 +0700

    Updates

---
+++ precache-manifest.72f6b5dc18688612aab929d7da192053.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfac1b02c742e79772ef27174d0de054",
+    "url": "/ndsappbeta/index.html"
+  },
+  {
+    "revision": "2599e674bce837a93dbe",
+    "url": "/ndsappbeta/static/css/main.524baaaa.chunk.css"
+  },
+  {
+    "revision": "aa19a2b937a7a75f78e7",
+    "url": "/ndsappbeta/static/js/2.088f27d3.chunk.js"
+  },
+  {
+    "revision": "35a4499e8a62806b5acef53c5fe4de88",
+    "url": "/ndsappbeta/static/js/2.088f27d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2599e674bce837a93dbe",
+    "url": "/ndsappbeta/static/js/main.31b7b5e5.chunk.js"
+  },
+  {
+    "revision": "b6c6f0baff810a1e1f84",
+    "url": "/ndsappbeta/static/js/runtime-main.537b08a8.js"
+  },
+  {
+    "revision": "7106cd73764c8d7dd24548fb52212d3f",
+    "url": "/ndsappbeta/static/media/Logosvg2.7106cd73.svg"
+  },
+  {
+    "revision": "fc71d46fa7e1621016b4467396cf05c1",
+    "url": "/ndsappbeta/static/media/darkBg2.fc71d46f.jpg"
+  },
+  {
+    "revision": "51621ce9f72b9a65e0f8b63942ebe97a",
+    "url": "/ndsappbeta/static/media/defaultBcg.51621ce9.jpg"
+  },
+  {
+    "revision": "c59b12ed3519e1b93b3eb8ac3737c6fe",
+    "url": "/ndsappbeta/static/media/defaultBg2.c59b12ed.jpg"
+  },
+  {
+    "revision": "73ead5f6e4b171aaf54747dd4b944a15",
+    "url": "/ndsappbeta/static/media/svgnds.73ead5f6.png"
+  },
+  {
+    "revision": "855c1879e1ba8d3faae2bbacec284059",
+    "url": "/ndsappbeta/static/media/videobgr1.855c1879.mp4"
+  },
+  {
+    "revision": "28fb8cebdd8027bb17e8327b281b6814",
+    "url": "/ndsappbeta/static/media/videobgr3.28fb8ceb.mp4"
+  },
+  {
+    "revision": "857b6cd675da5e2430fdfcda32405a79",
+    "url": "/ndsappbeta/static/media/videobgr5.857b6cd6.mp4"
+  }
+]);
